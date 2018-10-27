0bcd12f04fe7d94ecfd8287b30259c5e0a3f93b6
iBluebellz <31064313+iBluebellz@users.noreply.github.com>
2018-10-27 17:13:26 +0900

    add enum gamestate

---
+++ 3Dtest/static.h
@@ -0,0 +1,24 @@
+#pragma once
+
+enum GameState
+{
+	GameState_title_state_init,
+	GameState_title_state_running,
+	GameState_title_state_clean,
+	GameState_tutorial_state_init,
+	GameState_tutorial_state_running,
+	GameState_tutorial_state_clean,
+	GameState_game_state_init,
+	GameState_game_state_running,
+	GameState_game_state_clean,
+	GameState_result_state_init,
+	GameState_result_state_running,
+	GameState_result_state_clean,
+	GameState_naming_state_init,
+	GameState_naming_state_running,
+	GameState_naming_state_clean,
+	GameState_ranking_state_init,
+	GameState_ranking_state_running,
+	GameState_ranking_state_clean,
+	GameState_max
+};
