52eaebe9502932d1affd77b836a80392a530fc35
iBluebellz <31064313+iBluebellz@users.noreply.github.com>
2019-02-26 16:11:28 +0900

    Update new Texture

--- 3Dtest/GameResult.cpp
+++ 3Dtest/GameResult.cpp
@@ -1,13 +1,11 @@
 #include "stdafx.h"
 #include "GameResult.h"
 
-#define TEXTURE_BACKGROUND_PADDING	(20)
-
 GameResult::GameResult()
 {
 	bEnd = false;
 
-	texture_background = TEX_RESULT;
+	texture_background = TEX_RESULT1;
 
 	gColor = 0xffffffff;
 	counter = 0;
@@ -38,7 +36,7 @@ void GameResult::Update()
 
 void GameResult::Draw()
 {
-	Sprite_Draw_Background(texture_background,grade % 2 * (1000 + TEXTURE_BACKGROUND_PADDING), grade / 2 * (562 + TEXTURE_BACKGROUND_PADDING), 1000 - TEXTURE_BACKGROUND_PADDING, 570 - TEXTURE_BACKGROUND_PADDING);
+	Sprite_Draw_Background(texture_background);
 	pParticle->Draw();
 }
 
